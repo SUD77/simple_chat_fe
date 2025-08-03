c230e18afea16b12d065076d9c2e152d202cae16
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-08-03 13:40:18 +0530

    feat : set a chat box

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='bg-red-50'>
-      Hi there
+    <div className="h-screen bg-black">
+      <div className="h-[95vh]"></div>
+      <div className="w-full bg-white flex p-4">
+        <input className="flex-1 p-4"></input>
+        <button className="bg-purple-600 text-white p-4">Send Message</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
